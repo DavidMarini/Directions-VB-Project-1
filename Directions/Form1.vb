5cd45d5f7c0fab2e95f7376cca168da4d7bb3b4b
DavidMarini <33123403+DavidMarini@users.noreply.github.com>
2017-10-26 08:32:50 -0400

    Initial commit

---
+++ Directions/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class form1
+	'David Marini, Period 2
+	Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+		lblDirections.Visible = True
+	End Sub
+End Class
